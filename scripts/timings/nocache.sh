dd61b92c3db6f89ed9c724572ce58a0b853d99df
Nathan Morgan <knathan@mit.edu>
2024-06-25 17:05:45 -0400

    Squashed commit of the following:
    commit 4bf3b1f4a18af3a5291dc7112073339989669a12
    Author: Nathan Morgan <knathan@mit.edu>
    Date:   Tue Jun 25 17:00:32 2024 -0400
    
        _
    
    commit 2626c677f1a54223ac21aba978d08a8cf50efcda
    Author: Nathan Morgan <knathan@mit.edu>
    Date:   Tue Jun 25 16:57:26 2024 -0400
    
        _
    
    commit 0942098774c7e674f7c9e62d9e412c05b44ebb1c
    Author: Nathan Morgan <knathan@mit.edu>
    Date:   Tue Jun 25 16:56:25 2024 -0400
    
        add timing to scripts
    
    commit 5c71aa7943f3c31c39db3b5898790cf9620d50ef
    Author: Nathan Morgan <knathan@mit.edu>
    Date:   Tue Jun 25 13:22:13 2024 -0400
    
        fix off by one error
    
    commit 84083c9be5a8a1bd79043d79307cf4dac965d82f
    Author: Nathan Morgan <knathan@mit.edu>
    Date:   Mon Jun 24 16:32:54 2024 -0400
    
        add distributed scripts
    
    commit 7552df89677db3084ead6ad595c5bd93091fdc97
    Author: Nathan Morgan <knathan@mit.edu>
    Date:   Mon Jun 24 15:22:14 2024 -0400
    
        various changes
    
    commit 813cc40dd762d06d23ba159438758a95c9ed9ac8
    Author: Nathan Morgan <knathan@mit.edu>
    Date:   Mon Jun 24 14:05:33 2024 -0400
    
        change data sizes
    
    commit 0d50ea26576bef22faa39ac203bac55a0db77d47
    Author: Nathan Morgan <knathan@mit.edu>
    Date:   Thu Jun 20 16:52:48 2024 -0400
    
        update timing scripts
    
    commit 2a22019817f2bd31fbddd716bc98ef38d88a5ca9
    Author: Nathan Morgan <knathan@mit.edu>
    Date:   Mon Jun 10 14:09:26 2024 -0400
    
        initial script ideas

---
+++ scripts/timings/nocache.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+#SBATCH -J nocache
+#SBATCH -o nocache-%j.out
+#SBATCH -t 01:00:00
+#SBATCH --exclusive
+#SBATCH -N 1
+#SBATCH -p xeon-g6-volta
+
+save_dir=../results_timing/nocache
+data_dir=../../../data/timing
+
+mkdir $save_dir
+
+chemprop -h # Load and cache all the python packages for correct timing of the actual chemprop train call
+
+nvidia-smi \
+--query-gpu=index,timestamp,name,pstate,memory.used,utilization.gpu,utilization.memory,power.draw,temperature.gpu \
+--format=csv -l 10 > $save_dir/gpu_stats_train.csv &
+nvidia-smi --query-compute-apps=timestamp,pid,process_name,used_memory \
+--format=csv -l 10 > $save_dir/process_stats_train.csv &
+
+/usr/bin/time -v chemprop train \
+--data-path $data_dir/qm9_100k.csv \
+--splits-file $data_dir/100k_splits.json \
+--save-dir $save_dir \
+--depth 3 \
+--message-hidden-dim 300 \
+--ffn-num-layers 1 \
+--ffn-hidden-dim 300 \
+--num-workers 8 \
+--batch-size 64 \
+--accelerator gpu \
+--devices 1 \
+--epochs 50 \
+--activation RELU \
+--dropout 0.0 \
+--aggregation norm \
+--ensemble-size 1 \
+--task-type regression \
+--warmup-epochs 2 \
+--init-lr 0.0001 \
+--max-lr 0.001 \
+--final-lr 0.0001 \
+--data-seed 0 \
+--pytorch-seed 0 \
+--no-cache
